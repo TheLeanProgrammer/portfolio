043988ed0475b95da3a9b349f585bc9a8282e125
MadhavBahlMD <madhavbahl10@gmail.com>
2021-06-02 19:59:28 +0530

    Add boilerplate - 4th video

---
+++ src/assets/projectsData.js
@@ -0,0 +1,46 @@
+export const projectsData = [
+    {
+        projectName: 'Name It!',
+        projectDescription:
+            'Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!',
+        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj1.PNG',
+        videoUrl:
+            'https://www.youtube.com/playlist?list=PLGyA74h_S9NqJvvQ7-l9bUHHS6bMCkgo0',
+        projectUrl: 'http://theleanprogrammer.com/name-it/',
+    },
+    {
+        projectName: 'FlashType',
+        projectDescription:
+            'Having a good typing speed will always help you as a developer! Here, I present you the typing speed test application, where you can check your typing speed (in wpm) and share the results with your friends too.',
+        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj2.PNG',
+        videoUrl:
+            'https://www.youtube.com/playlist?list=PLGyA74h_S9NppHNrzUSjMQbnuHS9jlAcY',
+        projectUrl: 'http://theleanprogrammer.com/flashtype/',
+    },
+    {
+        projectName: 'Flexbox Cheatsheet',
+        projectDescription:
+            'A frontend web developer must know the concepts of CSS Flexbox. So here is a CSS Flexbox cheatsheet, built using CSS Flexbox. Learn the concepts of flexbox by building a cheatsheet which is built using flexbox. Super cool!',
+        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj3.PNG',
+        videoUrl:
+            'https://www.youtube.com/playlist?list=PLGyA74h_S9NrkKnaIrYeOolru4EakosHY',
+        projectUrl: 'http://theleanprogrammer.com/flex/',
+    },
+    {
+        projectName: 'PokeDex',
+        projectDescription:
+            "Pokemon was a huge part of our childhood, wasn't it? Talking about pokemon brings back so many memories, but do you remember the details of each pokemon? Probably No! So here I bring you a pokedex to check all pokemons :)",
+        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj4.PNG',
+        videoUrl:
+            'https://www.youtube.com/playlist?list=PLGyA74h_S9NrjM7mUiSmpKfUntwEcgMB3',
+        projectUrl: 'http://theleanprogrammer.com/pokedex/',
+    },
+    {
+        projectName: 'Portfolio Website',
+        projectDescription:
+            'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
+        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj5.PNG',
+        videoUrl: 'http://theleanprogrammer.com/aam',
+        projectUrl: 'http://madhavbahl.tech/portfolio/',
+    },
+];
