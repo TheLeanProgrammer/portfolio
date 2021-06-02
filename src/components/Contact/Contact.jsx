f19cd894edada38c4751b3433001b3331111afb0
MadhavBahlMD <madhavbahl10@gmail.com>
2021-06-02 21:04:18 +0530

    Add routes

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>I am the Contact page!</h1>
+        </div>
+    );
+};
+
+export default Contact;
