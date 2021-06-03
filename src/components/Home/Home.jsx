56b735678f3537fcfd8758bee1d7f5ef89f4b1fb
MadhavBahlMD <madhavbahl10@gmail.com>
2021-06-03 18:28:55 +0530

    Style the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import homeAnime from './../../assets/home_anime.gif';
 import './Home.css';
 
-const Home = () => {
-    return (
-        <div>
-            <h1>I am the home page!</h1>
-        </div>
-    );
-};
+class Home extends Component {
+    render() {
+        return (
+            <div className='home-container'>
+                <div className='header-text'>
+                    <h1>Welcome to my portfolio!</h1>
+                    <p>This is Madhav, a full stack dev and a teacher</p>
+                </div>
+                <div className='head-btns'>
+                    <Link to='/about' className='btn btn-white'>
+                        <p className='btn-text'>Know more about me</p>
+                    </Link>
+                    <Link to='/contact' className='btn btn-transparent'>
+                        <p className='btn-text'>Connect with me</p>
+                    </Link>
+                </div>
+                <div className='splash-image'>
+                    <img
+                        src={homeAnime}
+                        alt='animation'
+                        className='home-anime'
+                    />
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Home;
