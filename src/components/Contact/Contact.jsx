a2356bbc78774c4e2341ff0d849721e20a819011
MadhavBahlMD <madhavbahl10@gmail.com>
2021-06-04 20:45:08 +0530

    Start contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import FooterLink from '../FooterLink/FooterLink';
+import Header from '../Header/Header';
 import './Contact.css';
+import contactVector from './../../assets/contact_anime.png';
+import github from './../../assets/gh.png';
+import linkedin from './../../assets/li.png';
+import instagram from './../../assets/in.png';
+import medium from './../../assets/me.png';
+import web from './../../assets/web.png';
 
 const Contact = () => {
     return (
         <div className='section-container'>
-            <h1>I am the Contact page!</h1>
+            <Header
+                heading='Get in touch.'
+                details='Interested to collaborate? Feel free to drop me an email.'
+            />
+
+            {/* Social icons */}
+            <div className='social-icons-container'>
+                <a href='https://github.com/MadhavBahl' className='social-icon'>
+                    <img src={github} alt='social' />
+                </a>
+                <a
+                    href='https://linkedin.com/in/MadhavBahl'
+                    className='social-icon'
+                >
+                    <img src={linkedin} alt='social' />
+                </a>
+                <a
+                    href='https://instagram.com/theleanprogrammer'
+                    className='social-icon'
+                >
+                    <img src={instagram} alt='social' />
+                </a>
+                <a
+                    href='https://medium.com/@madhavbahl'
+                    className='social-icon'
+                >
+                    <img src={medium} alt='social' />
+                </a>
+                <a href='https://madhavbahl.tech' className='social-icon'>
+                    <img src={web} alt='social' />
+                </a>
+            </div>
+
+            <FooterLink
+                phrase='Read more '
+                toAdress='/about'
+                link='about me.'
+            />
+
+            <div className='vector-frame'>
+                <img
+                    src={contactVector}
+                    alt='vector'
+                    className='about-vector'
+                />
+            </div>
         </div>
     );
 };
