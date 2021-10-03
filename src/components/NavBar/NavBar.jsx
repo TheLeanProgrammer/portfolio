63e7fe7c5f8823f1c9d412b9ea1510e8cd0d609f
preetchani <preetchani23498@gmail.com>
2021-10-03 20:32:12 +0530

    Added feature to auto close the side bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,6 +5,25 @@ import { Link, withRouter } from 'react-router-dom';
 import './NavBar.css';
 
 class NavBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            menuOpen:false
+        }
+    }
+    
+    handleStateChange = (state) => {
+        this.setState({
+            menuOpen:state.isOpen
+        })
+    }
+
+    closeMenu = () => {
+        this.setState({
+            menuOpen:false
+        })
+    }
+
     render() {
         const { location } = this.props;
 
@@ -18,24 +37,27 @@ class NavBar extends React.Component {
             location.pathname === '/contact' ? 'active-item' : '';
 
         return (
-            <Menu>
+            <Menu 
+                isOpen={this.state.menuOpen}
+                onStateChange={(state)=>this.handleStateChange(state)}
+            >
                 {/* We NEVER want to use <a> inside our react router application */}
                 {/* <a id='home' className='menu-item' href='/'>
                     Home
                 </a> */}
-                <Link to='/' className={`menu-item ${homeClass}`}>
+                <Link to='/' onClick ={()=>this.closeMenu()} className={`menu-item ${homeClass}`}>
                     Home
                 </Link>
-                <Link to='/about' className={`menu-item ${aboutClass}`}>
+                <Link to='/about' onClick ={()=>this.closeMenu()} className={`menu-item ${aboutClass}`}>
                     About
                 </Link>
-                <Link to='/projects' className={`menu-item ${projectsClass}`}>
+                <Link to='/projects' onClick ={()=>this.closeMenu()} className={`menu-item ${projectsClass}`}>
                     Projects
                 </Link>
-                <Link to='/skills' className={`menu-item ${skillsClass}`}>
+                <Link to='/skills' onClick ={()=>this.closeMenu()} className={`menu-item ${skillsClass}`}>
                     Skills
                 </Link>
-                <Link to='/contact' className={`menu-item ${contactClass}`}>
+                <Link to='/contact' onClick ={()=>this.closeMenu()} className={`menu-item ${contactClass}`}>
                     Contact
                 </Link>
             </Menu>
